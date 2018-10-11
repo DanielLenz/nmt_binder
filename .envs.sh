bcdb1e651741c1e704882f73daea256234f75177
Daniel Lenz <mail@daniellenz.org>
2018-10-11 23:29:08 +0200

    Declare build dir in .envs.sh

--- .envs.sh
+++ .envs.sh
@@ -1,4 +1,7 @@
 # Set all paths and compiler flags
+
+export BUILD_DIR=$HOME/build_dir
+
 export PATH=$HOME/software/libsharp/auto/bin:$BUILD_DIR/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BUILD_DIR/lib:$HOME/software/libsharp/auto/lib:/usr/local/lib
 export LDFLAGS="-L$BUILD_DIR/lib -L$HOME/software/libsharp/auto/lib -L/usr/local/lib"
